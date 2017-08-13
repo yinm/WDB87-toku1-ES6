d6efffb3ae5aa762b431d4c5354dec57234f9c85
yinm <yinm.cminor9@gmail.com>
2017-08-13 12:46:01 +0900

    try default import/export

---
+++ chapter6/ES5/default-export/default-import.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var _foo = require("./foo");
+
+var _foo2 = _interopRequireDefault(_foo);
+
+var _bar = require("./bar");
+
+var _bar2 = _interopRequireDefault(_bar);
+
+var _baz = require("./baz");
+
+var _baz2 = _interopRequireDefault(_baz);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log(_foo2.default);
+(0, _bar2.default)();
+new _baz2.default();
---
+++ chapter6/ES6/default-export/default-import.js
@@ -0,0 +1,7 @@
+import a from "./foo";
+import b from "./bar";
+import c from "./baz";
+
+console.log(a);
+b();
+new c();

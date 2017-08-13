68889c941123363b0aef3f85b888b7999dc50779
yinm <yinm.cminor9@gmail.com>
2017-08-13 10:52:59 +0900

    try omission properties

---
+++ chapter3/ES5/omission-properties.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var foo = 0,
+    bar = 1;
+
+var obj = { foo: foo, bar: bar };
+console.log(obj);
---
+++ chapter3/ES6/omission-properties.js
@@ -0,0 +1,4 @@
+let foo = 0, bar = 1;
+
+let obj = {foo, bar};
+console.log(obj);

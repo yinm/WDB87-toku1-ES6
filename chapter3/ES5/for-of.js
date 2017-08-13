6e440701b66b459e7db15292a5a25cbae06f1f4a
yinm <yinm.cminor9@gmail.com>
2017-08-13 11:51:51 +0900

    try for of

---
+++ chapter3/ES5/for-of.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var iter = [1, 2, 3];
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = iter[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var n = _step.value;
+
+    console.log(n);
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+var chars = [];
+var _iteratorNormalCompletion2 = true;
+var _didIteratorError2 = false;
+var _iteratorError2 = undefined;
+
+try {
+  for (var _iterator2 = "foo"[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+    var _n = _step2.value;
+
+    chars.push(_n);
+  }
+} catch (err) {
+  _didIteratorError2 = true;
+  _iteratorError2 = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion2 && _iterator2.return) {
+      _iterator2.return();
+    }
+  } finally {
+    if (_didIteratorError2) {
+      throw _iteratorError2;
+    }
+  }
+}
+
+console.log(chars);
---
+++ chapter3/ES6/for-of.js
@@ -0,0 +1,10 @@
+const iter = [1, 2, 3];
+for (let n of iter) {
+  console.log(n);
+}
+
+let chars = [];
+for (let n of "foo") {
+  chars.push(n);
+}
+console.log(chars);

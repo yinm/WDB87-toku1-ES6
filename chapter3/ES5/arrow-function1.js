99fcaa510acb0592ae7a2f060f201e9515f8b7f9
yinm <yinm.cminor9@gmail.com>
2017-08-13 10:26:47 +0900

    try arrow function

---
+++ chapter3/ES5/arrow-function1.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var add = function add(a, b) {
+  return a + b;
+};
+
+// ブロックの波括弧とreturnを省略
+var add2 = function add2(a, b) {
+  return a + b;
+};
+
+// 引数の丸括弧を省略
+var square = function square(n) {
+  return n * n;
+};
---
+++ chapter3/ES6/arrow-function1.js
@@ -0,0 +1,9 @@
+let add = (a, b) => {
+  return a + b;
+};
+
+// ブロックの波括弧とreturnを省略
+let add2 = (a, b) => a + b;
+
+// 引数の丸括弧を省略
+let square = n => n * n;
